8aaf6215190984647d5f097aa9f30a0a6d2489bf
Hieu Hoang <you@example.com>
2024-03-06 05:37:32 +0700

    Add automatical sorting and fix spelling

---
+++ New folder/app.h
@@ -0,0 +1,31 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+struct staff
+{
+    string staff_id;
+    string staff_name;
+    string relationship;
+    short childrent_number = 0;
+    string education_level;
+    int base_salary = 0;
+    short absent_legal = 0;
+    short absent_illegal = 0;
+    short ot_day = 0;
+    string result[2];
+    double income;
+};
+
+vector<staff>& removed_employee_to_list(vector<staff>& list);
+vector<staff>& add_employee_to_list(vector<staff>& list);
+void show_salary_of_staff(staff employee);
+void show_detail(vector<staff> list, int index);
+void show_detail(staff employee);
+void choice_option_1();
+void Choice_table();
+staff creater_staff();
+double net_salary(staff employee);
+int search(vector<staff> list);
+vector<staff>& sorting( vector<staff>& LIST);
---
+++ New folder/main.cpp
@@ -0,0 +1,37 @@
+#include "app.h"
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+int choice_flag = 0;
+
+int main() {
+    vector<staff> list_employ;
+    while (1) {
+        Choice_table();
+        cout << "Nhap lua chon: " << endl;
+        cin >> choice_flag;
+        switch (choice_flag)
+        {
+        case 1:
+            choice_option_1();
+            int tflag;
+            cin >> tflag;
+            if (tflag == 1) add_employee_to_list(list_employ);
+            if (tflag == 2) removed_employee_to_list(list_employ);
+            break;
+        case 2:
+            for (auto v : list_employ) {
+                show_salary_of_staff(v);
+            }
+        case 3:
+            cout << " Nhap ma nhan vien can tim kiem: ";
+            show_detail(list_employ, search(list_employ));
+            break;
+            
+        default:
+            break;
+        }
+    }
+    return 0;
+}
